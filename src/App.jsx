2528e2a1b6ceb732b31b027a6ee7c36d3afcec88
JaycobHoffman1 <jaycobhoffman2004@gmail.com>
2024-10-09 22:22:06 -0700

    final commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+// Task 4: Integrating Components and Updating the User Interface
+import CharacterList from './character-list';
+function App() {
+  return (
+    <div>
+      <CharacterList />
+    </div>
+  );
+};
+export default App;
